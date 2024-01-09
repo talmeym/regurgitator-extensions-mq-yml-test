8c4cfd3c98b791a68e43c5ba160a6725198fe076
talmeym <mtalmey@gmail.com>
2024-01-09 13:34:28 +0000

    emarte uk

--- src/test/java/uk/emarte/regurgitator/test/CreateJmsResponseYmlLoaderTest.java
+++ src/test/java/uk/emarte/regurgitator/test/CreateJmsResponseYmlLoaderTest.java
@@ -2,13 +2,13 @@
  * Copyright (C) 2017 Miles Talmey.
  * Distributed under the MIT License (license terms are at http://opensource.org/licenses/MIT).
  */
-package com.emarte.regurgitator.test;
+package uk.emarte.regurgitator.test;
 
-import com.emarte.regurgitator.extensions.mq.CreateJmsResponseYmlLoader;
+import uk.emarte.regurgitator.extensions.mq.CreateJmsResponseYmlLoader;
 import org.junit.Test;
 
-import static com.emarte.regurgitator.core.ConfigurationFile.loadFile;
-import static com.emarte.regurgitator.test.MqExtensionsLoaderTestExpectations.*;
+import static uk.emarte.regurgitator.core.ConfigurationFile.loadFile;
+import static uk.emarte.regurgitator.test.MqExtensionsLoaderTestExpectations.*;
 
 public class CreateJmsResponseYmlLoaderTest extends YmlLoaderTest {
     public CreateJmsResponseYmlLoaderTest() {
